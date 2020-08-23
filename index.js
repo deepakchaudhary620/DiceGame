631a615337747ece39829af915e978c58c95488d
deepakchaudhary620 <68245967+deepakchaudhary620@users.noreply.github.com>
2020-08-23 10:47:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1= Math.floor(Math.random()*6)+1;
-var randomImage1 = "images/" + "dice"+randomNumber1+".png";
+var randomImage1 = "dice"+randomNumber1+".png";
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src" ,randomImage1);
 
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomImage2 = "images/dice"+randomNumber2+".png";
+var randomImage2 = "dice"+randomNumber2+".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImage2);
 
